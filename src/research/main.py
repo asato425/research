5bd39166da0bb1de0049837c71ca9f45611a878a
asato425 <suasa425@icloud.com>
2025-09-03 01:10:48 +0900

    main関数のリファクタリングを行い、GitHubToolとLinterToolの初期化を整理し、サンプルリポジトリの情報を取得して表示する機能を追加

--- src/research/main.py
+++ src/research/main.py
@@ -1,26 +1,18 @@
 from research.tools.github import GitHubTool
 from research.tools.linter import LinterTool
 from research.parsing.workflow_log_parser import workflow_log_parser
+from research.parsing.linter_parser import lintresult_parser
 
 def main():
-    github = GitHubTool()
-    #linter = LinterTool()
-    #file_name = "test.py"
-    repo_url = "https://github.com/asato425/test"
-
-    # clone_result = github.clone_repository(repo_url=repo_url)
-    # if clone_result.status != "success":
-    #     return
-
-    local_path = "~/Desktop/research_clones/test"
-    # local_path = clone_result.local_path
+    log_is = True
+    github = GitHubTool(log_is=log_is)
+    linter = LinterTool(log_is=log_is)
     
-    push_result = github.commit_and_push(local_path=local_path, message="test commit")
-    workflow_result = github.get_latest_workflow_logs(repo_url=repo_url, commit_sha=push_result.commit_sha)
+    file_name = "sample.yml"
+    repo_url = "https://github.com/fastapi/fastapi"
 
-    workflow_log_parse_result = workflow_log_parser(workflow_result)
-    print("--------------------------------------------------")
-    print(workflow_log_parse_result)
-    # github.delete_cloned_repository(local_path=local_path)
+    repo_info = github.get_repository_info(repo_url=repo_url)
+    print("Repository Info:", repo_info)
+
 if __name__ == "__main__":
     main()
