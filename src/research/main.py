739bd98bd5aa739b1a23600118c93098fd11d573
asato425 <suasa425@icloud.com>
2025-09-05 00:10:07 +0900

    LLMToolとRAGToolのインスタンスをmain関数内で初期化し、モデル生成とRAG処理を追加しました。

--- src/research/main.py
+++ src/research/main.py
@@ -1,16 +1,21 @@
 from research.tools.github import GitHubTool
 from research.tools.linter import LinterTool
 from research.tools.parser import ParserTool
+from research.tools.llm import LLMTool
+from research.tools.rag import RAGTool
 
 def main():
     log_is = True
     github = GitHubTool(log_is=log_is)
     linter = LinterTool(log_is=log_is)
     parser = ParserTool(log_is=log_is)
+    llm = LLMTool(log_is=log_is)
+    rag = RAGTool(log_is=log_is)
 
     file_name = "sample.yml"
     repo_url = "https://github.com/fastapi/fastapi"
-
+    llm = llm.create_model()
+    rag.rag_git()
     repo_info = github.get_repository_info(repo_url=repo_url)
     
 
