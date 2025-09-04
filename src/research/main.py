9d8ca71cf4c95c80745bb472f07517792ac056ac
asato425 <suasa425@icloud.com>
2025-09-04 16:28:43 +0900

    ParserToolのインポートを修正し、不要なインポートを削除

--- src/research/main.py
+++ src/research/main.py
@@ -1,18 +1,18 @@
 from research.tools.github import GitHubTool
 from research.tools.linter import LinterTool
-from research.parsing.workflow_log_parser import workflow_log_parser
-from research.parsing.linter_parser import lintresult_parser
+from research.tools.parser import ParserTool
 
 def main():
     log_is = True
     github = GitHubTool(log_is=log_is)
     linter = LinterTool(log_is=log_is)
-    
+    parser = ParserTool(log_is=log_is)
+
     file_name = "sample.yml"
     repo_url = "https://github.com/fastapi/fastapi"
 
     repo_info = github.get_repository_info(repo_url=repo_url)
-    print("Repository Info:", repo_info)
+    
 
 if __name__ == "__main__":
     main()
