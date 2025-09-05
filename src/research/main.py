dddde1dc1e9a3e8d20b64ae5c9b8cf4fdd8344a3
asato425 <suasa425@icloud.com>
2025-09-06 00:44:30 +0900

    不要なLinterToolとParserToolのインポートを削除し、gpt_agentの作成と呼び出しを追加しました。

--- src/research/main.py
+++ src/research/main.py
@@ -1,23 +1,20 @@
 from research.tools.github import GitHubTool
-from research.tools.linter import LinterTool
-from research.tools.parser import ParserTool
 from research.tools.llm import LLMTool
 from research.tools.rag import RAGTool
 
 def main():
     log_is = True
     github = GitHubTool(log_is=log_is)
-    linter = LinterTool(log_is=log_is)
-    parser = ParserTool(log_is=log_is)
     llm = LLMTool(log_is=log_is)
     rag = RAGTool(log_is=log_is)
 
-    file_name = "sample.yml"
-    repo_url = "https://github.com/fastapi/fastapi"
-    llm = llm.create_model()
-    rag.rag_git()
-    repo_info = github.get_repository_info(repo_url=repo_url)
-    
+    gpt_agent = llm.create_agent(
+        model_name = "gpt-4",
+        tools = [
+            llm.retriever_to_tool(rag.rag_tavily(max_results=3), "tavily", "ネット検索し、情報を提供します。")
+        ],
+    )
 
+    print(gpt_agent.invoke({"input": "明日の東京の天気を教えてください"}))
 if __name__ == "__main__":
     main()
