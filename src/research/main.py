4d48d13c82da5721ebaa5f393bc90cf66dd16d97
asato425 <suasa425@icloud.com>
2025-09-12 13:58:34 +0900

    main関数内でのlog_is引数の削除とset_log_is関数の呼び出しを追加しました。また、GitHubTool、LLMTool、RAGToolの初期化時にlog_is引数を削除しました。

--- src/research/main.py
+++ src/research/main.py
@@ -1,20 +1,14 @@
 from research.tools.github import GitHubTool
 from research.tools.llm import LLMTool
 from research.tools.rag import RAGTool
+from research.log_output.log import set_log_is
 
 def main():
-    log_is = True
-    github = GitHubTool(log_is=log_is)
-    llm = LLMTool(log_is=log_is)
-    rag = RAGTool(log_is=log_is)
-
-    gpt_agent = llm.create_agent(
-        model_name = "gpt-4",
-        tools = [
-            llm.retriever_to_tool(rag.rag_tavily(max_results=3), "tavily", "ネット検索し、情報を提供します。")
-        ],
-    )
-
-    print(gpt_agent.invoke({"input": "明日の東京の天気を教えてください"}))
+    set_log_is(False)
+    github = GitHubTool()
+    llm = LLMTool()
+    rag = RAGTool()
+    repo_url = "https://github.com/asato425/test"
+    github.get_repository_info(repo_url)
 if __name__ == "__main__":
     main()
